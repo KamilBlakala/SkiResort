bc29d3ee2995567a02a7d7e060f8e1de8c2a4831
KamilBlakala <kamil.blakala@gmail.com>
2023-01-24 21:21:14 +0100

    First styling of nav

--- src/js/parts/burgerNav.js
+++ src/js/parts/burgerNav.js
@@ -1,8 +1,10 @@
 const brgBars = document.querySelector('.burger-btn__box');
 const navBtn = document.querySelector('.burger-btn');
+const navHandler = document.querySelector('.navListItems');
 
 const handleNav = () => {
 	brgBars.classList.toggle('open');
+	navHandler.classList.toggle('.active');
 };
 
 navBtn.addEventListener('click', handleNav);
